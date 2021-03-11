d1ddd4cd25209cc99e1f25c1cae14dd705dcc92d
Michal Vlasák <daeatel@gmail.com>
2021-03-11 21:30:52 +0100

    ♻ Change name and mail in seeds

--- src/test/utils/seed.ts
+++ src/test/utils/seed.ts
@@ -1,7 +1,7 @@
 export default async (app: any) => {
   const [user, user2] = await Promise.all([
     app.service('users').create({ email: 'auth@user.com', name: 'John Doe' }),
-    app.service('users').create({ email: 'auth2@user.com', name: 'Jane Doe' }),
+    app.service('users').create({ email: 'dana@user.com', name: 'Dana Scully' }),
   ])
   const book = await app.service('books').create({ title: 'What to do now?' })
   return { user, user2, book }
